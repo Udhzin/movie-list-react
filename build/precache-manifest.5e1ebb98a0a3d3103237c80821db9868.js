d5d9a8304286f6c524b4e0e72546c578cdb175b6
Udzhin <udhzin@gmail.com>
2020-05-28 16:16:19 +0300

    add scss

---
+++ build/precache-manifest.5e1ebb98a0a3d3103237c80821db9868.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32c163446180fcd1c31273000d1193a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d2104a1ba0dab9ae79e",
+    "url": "/static/css/main.a689ad72.chunk.css"
+  },
+  {
+    "revision": "35164506518bf81bdc85",
+    "url": "/static/js/2.e9cbdc6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9cbdc6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2104a1ba0dab9ae79e",
+    "url": "/static/js/main.c21ee938.chunk.js"
+  },
+  {
+    "revision": "c486044b3a856f465f59",
+    "url": "/static/js/runtime-main.720b8280.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
