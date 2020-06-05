4fd5acc6c2a6dbcce50b2e9f2bd19e25518a13f1
Udzhin <udhzin@gmail.com>
2020-06-05 19:09:25 +0300

    Updates

---
+++ precache-manifest.d66b4ce7a264eb3456fed90db1658ede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ba716474b500c8aa1c3b4ff036784c",
+    "url": "/movie-list-react/index.html"
+  },
+  {
+    "revision": "3ade2db7c8c1bb0eb0b3",
+    "url": "/movie-list-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b1f167287422e2287d18",
+    "url": "/movie-list-react/static/css/main.fbbb2e84.chunk.css"
+  },
+  {
+    "revision": "3ade2db7c8c1bb0eb0b3",
+    "url": "/movie-list-react/static/js/2.feca682d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-list-react/static/js/2.feca682d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f167287422e2287d18",
+    "url": "/movie-list-react/static/js/main.d45cbca8.chunk.js"
+  },
+  {
+    "revision": "1e29e052bf0418bdab5c",
+    "url": "/movie-list-react/static/js/runtime-main.e99bc905.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-test/precache-manifest.c7d37e21ff4ed9d90ec0b2d1e678fd49.js"
+  "/movie-list-react/precache-manifest.d66b4ce7a264eb3456fed90db1658ede.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie-list-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
