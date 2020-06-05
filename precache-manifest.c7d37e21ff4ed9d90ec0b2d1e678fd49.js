7684643772e6ffb57574bf2608c9d44eb8e0b711
Udzhin <udhzin@gmail.com>
2020-06-05 18:44:31 +0300

    Updates

---
+++ precache-manifest.c7d37e21ff4ed9d90ec0b2d1e678fd49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b4c48da3ac167ce5b7aaab03f6b7c4",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "76bf106340d3c1d8f3d9",
+    "url": "/react-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d2c7f7595fb2e4a61e11",
+    "url": "/react-test/static/css/main.fbbb2e84.chunk.css"
+  },
+  {
+    "revision": "76bf106340d3c1d8f3d9",
+    "url": "/react-test/static/js/2.42323821.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.42323821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c7f7595fb2e4a61e11",
+    "url": "/react-test/static/js/main.69e7a253.chunk.js"
+  },
+  {
+    "revision": "8f7613e88fa89e296daa",
+    "url": "/react-test/static/js/runtime-main.9261beab.js"
+  }
+]);
